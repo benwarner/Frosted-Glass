be8b0bb1d85eea9b360ea99bea4856f7a2104abd
Ben Warner <latticereduced@gmail.com>
2011-11-01 21:25:08 -0600

    I am committing A FELONY!

---
+++ ex19_2.py
@@ -0,0 +1,17 @@
+from sys import argv
+script, p = argv
+p = int(p)
+
+def primetester(p, k):
+    return k**(p-1) % p == 1
+
+prime = True
+
+for k in range(p-1):
+    if primetester(p, k):
+        continue
+    else:
+        prime = False
+        break
+
+print prime
